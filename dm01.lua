4c02dacb8477469d7cdfa783eb193a981459f9cf
neighbor <enumhack@163.com>
2019-03-27 16:44:32 +0800

    测试函数作为数组的值

--- dm01.lua
+++ dm01.lua
@@ -1,2 +1,16 @@
 --第一个lua程序
 print("hello world")
+
+function encode()
+    print("hhh")
+end
+
+local array = {
+    [2001] = encode -- 指定索引为2001时，值为encode函数
+}
+
+print(array[2001]())
+
+for k, v in pairs(array) do
+    print(k, v)
+end
